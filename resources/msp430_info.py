7e19b915f3f8b4234711501205eb365d1c2ebfd2
Rice Rodriguez <rice.rodriguez@ttu.edu>
2019-07-31 10:39:11 -0500

    Added regular expression checking

--- resources/msp430_info.py
+++ resources/msp430_info.py
@@ -20,6 +20,37 @@ Registers = {
     'SR'     : '0010'
 }
 
+Addressing_Prefixes = {
+    '([0-9])\((R[0-9]{,2})\)'            : 'INDEXED',
+    '#0x(([0-9]){1,4})'    : 'IMMEDIATE',
+    '#(([0-9])+)'          : 'IMMEDIATE',
+    '#(?a)'              : 'ABSOLUTE',
+    '0x(?a)'             : 'SYMBOLIC',
+    '&(?a)'              : 'ABSOLUTE',
+    '@\((R[0-9]{,2})\)\+'            : 'INDIRECT AUTOINCREMENT',
+    '@\((R[0-9]{,2})\)'              : 'INDIRECT'
+}
+
+As_Modes = {
+    'REGISTER'  : '00',
+    'INDEXED'   : '01',
+    'SYMBOLIC'  : '01',
+    'ABSOLUTE'  : '01',
+    'INDIRECT'  : '10',
+    'IMMEDIATE' : '11',
+    'INDIRECT AUTOINCREMENT' : '11'
+}
+
+Addressing_SRegs = {
+    'REGISTER'  : None,
+    'INDEXED'   : None,
+    'SYMBOLIC'  : 'PC',         # X is TONI - PC
+    'ABSOLUTE'  : 'SR',
+    'INDIRECT'  : None,
+    'IMMEDIATE' : 'PC',
+    'INDIRECT AUTOINCREMENT' : None
+}
+
 Directives = {
     'ORG'    : -1,
     'DW'     : 2,
@@ -27,7 +58,6 @@ Directives = {
     'DS'     : -1,
     'EQU'    : 0
 }
-
 
 __opcodes_1 = {
     'mov'    : '0100',
@@ -80,24 +110,22 @@ __opcodes_emu = {
     'clr'    : ['2','mov R3, ${dst}'],
     'clrc'   : ['4','and #0xFFFE, SR'],
     'dadc'   : ['2','dadd R3, ${dst}'],
-    'dec'    : ['4','sub 0(R3), ${dst}'],
-    'decd'   : ['4','sub @R3, ${dst}'],
+    'dec'    : ['2','sub 0(R3), ${dst}'],
+    'decd'   : ['2','sub @R3, ${dst}'],
     'dint'   : ['4','and #0xFFF7, SR'],
     'eint'   : ['4','bis #0x0008, SR'],
-    'inc'    : ['4','add 0(R3), ${dst}'],
-    'incd'   : ['4','add @R3, ${dst}'],
+    'inc'    : ['2','add 0(R3), ${dst}'],
+    'incd'   : ['2','add @R3, ${dst}'],
     'inv'    : ['4','xor #0xFFFF, ${dst}'],
     'pop'    : ['2','mov @SP+, ${dst}'],
     'ret'    : ['2','mov @SP+, PC'],
     'rla'    : ['2','add ${dst}, ${dst}'],
     'rlc'    : ['2','addc ${dst}, ${dst}'],
-    'sbc'    : ['4','subc #0x0, ${dst}'],
-    'setc'   : ['4','bis #0x1, SR'],
-    'setn'   : ['4','bis #0x4, SR'],
-    'setz'   : ['4','bis #0x2, SR'],
+    'sbc'    : ['2','subc #0x0, ${dst}'],
+    'setc'   : ['2','bis #0x1, SR'],
+    'setn'   : ['2','bis #0x4, SR'],
+    'setz'   : ['2','bis #0x2, SR'],
     'tst'    : ['2','cmp R3, ${dst}']
 }
 
-OPCODES = [__opcodes_1, __opcodes_2, __opcodes_3, __opcodes_emu]
-
-
+OPCODES = [__opcodes_1, __opcodes_2, __opcodes_3, __opcodes_emu]        
